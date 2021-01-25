d33074370fe845bd5fc78b16241d63b1efb36929
Yossi Segev <yossi@tailorbrands.com>
2021-01-25 17:04:10 +0200

    added script and stuff

---
+++ auto-readme/generate.rb
@@ -0,0 +1,52 @@
+require "yaml"
+
+@template = File.read("template.md")
+valuesFile = YAML.load(File.read("values.yml"))
+@values = {}
+valuesFile.each { | key, value |
+    @values[key.to_sym] = value
+}
+
+pattern = /{{(.*?)}}/
+placeholders = []
+
+@template.each_line do |line|
+    if line =~ pattern
+        placeholders.push(line.slice! pattern)
+    end
+end
+
+finalText = @template.clone
+placeholders.each do |placeholder|
+    key =  placeholder.tr('{{}}', '').to_sym
+    finalText = finalText.sub(placeholder, @values[key])
+end
+
+puts finalText
+File.open("README.md", "w") { |f| f.write(finalText) }
+
+
+
+
+
+
+
+
+# if (File.file?("README.md"))
+#     File.write("README.md", finalText)
+# end
+# toReplace = @template.find_all(/{(.*?)}/)
+# puts toReplace[1].class
+
+# toReplace.each do |string|
+#     string2 = "{#{string[0]}}"
+#     puts string2
+#     content = @template.sub(string2, @config[:string[0]])
+#     puts content
+# end
+# newContent = @template.sub(/{title}/, "hi")
+# puts newContent
+
+# @template.each_line do |line|
+#     puts line
+# end
---
+++ auto-readme/template.md
@@ -0,0 +1,11 @@
+# {{title}}
+{{description}}
+
+## Activity
+[Latest releases](https://github.com/TailorBrands/{{repository_name}}/actions?query=workflow%3ARelease)
+[Latest merged Pull Requests](https://github.com/TailorBrands/{{repository_name}}/pulls?q=is%3Apr+is%3Aclosed+is%3Amerged+)
+
+## Some Code block
+```
+conjure raise {{repository_name}}
+```
