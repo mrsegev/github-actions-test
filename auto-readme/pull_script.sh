351855ca60d1ba97e6804b5ba1040bc4962ce265
Yossi Segev <yossi@tailorbrands.com>
2021-03-07 14:45:19 +0200

    wip00

--- auto-readme/pull_script.sh
+++ auto-readme/pull_script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Adding maker submodule
-git submodule add git@github.com:mrsegev/auto-readme-script.git
+git submodule add https://github.com/mrsegev/auto-readme-script.git
 git submodule update --init --recursive
 git submodule update --remote --recursive
